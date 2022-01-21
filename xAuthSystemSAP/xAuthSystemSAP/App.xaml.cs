53524996bf0bfd6a193e261768bd9a72913569bd
veeglazz tonny <veeglazz@gmail.com>
2022-01-21 10:27:43 +0700

    Add project files.

---
+++ xAuthSystemSAP/xAuthSystemSAP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using xAuthSystemSAP.Services;
+using xAuthSystemSAP.Views;
+
+namespace xAuthSystemSAP
+{
+    public partial class App : Application
+    {
+        public static string BaseAPI { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            BaseAPI = "https://103.111.202.38:1818/";
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
